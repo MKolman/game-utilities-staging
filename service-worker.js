838b5f858d75706eb42a996c5f00b547a7544e1d
Maks Kolman <kolman.maks@gmail.com>
2019-07-21 18:22:31 +0100

    Enable PWA

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v2';
+
+const FILES_TO_CACHE = [
+  'index.html', 'js/draw.js', 'js/words.js', 'js/actions.js', 'style.css',
+  'manifest.json', 'img/logo_256.png'];
+
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  // Precache static resources here.
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  // Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+
+  evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                console.log('[ServiceWorker] cache put', evt.request.url);
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            }).catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+      }));
+});
