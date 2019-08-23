d78d836c244cfc77177f3e3b0dd1ef2f1911e40c
Maks Kolman <kolman.maks@gmail.com>
2019-08-23 22:02:33 +0100

    Updates

---
+++ precache-manifest.3619c474aad247cb403f859b348fe088.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "23020039904e693a0db9",
+    "url": "/css/app.4ef84f9e.css"
+  },
+  {
+    "revision": "23020039904e693a0db9",
+    "url": "/js/app.bd81a0f6.js"
+  },
+  {
+    "revision": "6f61d5c9b8a5058b4ddd",
+    "url": "/css/chunk-510f685a.db8aa32d.css"
+  },
+  {
+    "revision": "6f61d5c9b8a5058b4ddd",
+    "url": "/js/chunk-510f685a.ec569389.js"
+  },
+  {
+    "revision": "2c43c900481edefa7832",
+    "url": "/css/chunk-7f85c750.2e85991b.css"
+  },
+  {
+    "revision": "2c43c900481edefa7832",
+    "url": "/js/chunk-7f85c750.adddd724.js"
+  },
+  {
+    "revision": "422c5a90b909d521011d",
+    "url": "/css/chunk-881509b4.3f3c9131.css"
+  },
+  {
+    "revision": "422c5a90b909d521011d",
+    "url": "/js/chunk-881509b4.fb7e0bf5.js"
+  },
+  {
+    "revision": "29ef080617204e697654",
+    "url": "/css/chunk-b45bd6d0.772a5efc.css"
+  },
+  {
+    "revision": "29ef080617204e697654",
+    "url": "/js/chunk-b45bd6d0.b00b93a8.js"
+  },
+  {
+    "revision": "8d2d52ae3674af792d72",
+    "url": "/js/chunk-vendors.3a422538.js"
+  },
+  {
+    "revision": "678d74b98edf8e529e5cbb05bb3d5c8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9c71c7514c4036f0122b1bdc202c17",
+    "url": "/img/en_flag.svg"
+  },
+  {
+    "revision": "c7f8bb0d07a3e1a92d9374be4bab1192",
+    "url": "/img/si_flag.svg"
+  },
+  {
+    "revision": "373c3c397fdace8ce297bef3492cb905",
+    "url": "/img/unique/unique_rules_1.svg"
+  },
+  {
+    "revision": "c8ad859373dd5ee9003f373f6ad4b9ef",
+    "url": "/img/unique/unique_rules_4.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4d83d0daecad353ce69a81b47b47268b",
+    "url": "/img/unique/unique_rules_2.svg"
+  },
+  {
+    "revision": "0c1f1eca1f68a7a9ec252e35d19b7aa0",
+    "url": "/img/unique/unique_rules_3.svg"
+  },
+  {
+    "revision": "2463adeeffd22d9ae6aadc48c90592b1",
+    "url": "/img/unique/unique_rules_5.svg"
+  }
+];
