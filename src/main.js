9f2f1daa89c70f98553a963da406e9fcde5e64b7
Maks Kolman <kolman.maks@gmail.com>
2019-08-24 12:43:22 +0100

    Lint

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
-import VueAnalytics from 'vue-analytics'
+import VueAnalytics from "vue-analytics";
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
 Vue.use(VueAnalytics, {
-  id: 'UA-42885055-4',
+  id: "UA-42885055-4",
   router
-})
+});
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/game-vue/'
-}
+  publicPath: "/game-vue/"
+};
