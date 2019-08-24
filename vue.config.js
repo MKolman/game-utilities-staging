fc04880c088272856fa9f5b028c8c9dcf8244659
Maks Kolman <kolman.maks@gmail.com>
2019-08-24 13:09:08 +0100

    Fix PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: "/game-vue/"
+  publicPath: "/game-vue/",
+  pwa: {
+    themeColor: "#D32F2F",
+    backgroundColor: "#EEEEEE",
+    name: "Play Now",
+    msTileColor: "#EEEEEE"
+  }
 };
