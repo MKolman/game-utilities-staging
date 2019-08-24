430e4977e1b602a7ca1f36e59d2d2e2fa2489336
Maks Kolman <kolman.maks@gmail.com>
2019-08-24 23:13:27 +0100

    Upgrade vue cli to generate manifest on build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,46 @@
 module.exports = {
   publicPath: process.env.BASE_URL,
+
   pwa: {
-    themeColor: "#D32F2F",
-    backgroundColor: "#EEEEEE",
-    name: "Play Now",
-    msTileColor: "#EEEEEE"
+    themeColor: process.env.VUE_APP_THEME,
+    name: process.env.VUE_APP_TITLE,
+    msTileColor: process.env.VUE_APP_BGCOLOR,
+    manifestOptions: {
+      background_color: process.env.VUE_APP_BGCOLOR,
+      icons: [
+        {
+          src: "./img/icons/android-chrome-128x128.png",
+          sizes: "128x128",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-152x152.png",
+          sizes: "152x152",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-256x256.png",
+          sizes: "256x256",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-1280x1280.png",
+          sizes: "1280x1280",
+          type: "image/png"
+        }
+      ],
+      start_url: "./index.html?utm_src=pwa",
+      orientation: "natural"
+    }
   }
 };
