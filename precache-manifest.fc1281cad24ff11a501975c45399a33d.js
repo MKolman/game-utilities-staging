786ac2425356c64735fdf083236ad2409c742579
Maks Kolman <kolman.maks@gmail.com>
2019-08-31 22:49:00 +0100

    Updates

---
+++ precache-manifest.fc1281cad24ff11a501975c45399a33d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3afc0959f643623293",
+    "url": "/game-utilities-staging/css/app.fad1b45f.css"
+  },
+  {
+    "revision": "671a7de38eea3dfaf916",
+    "url": "/game-utilities-staging/css/chunk-37587e1a.2e4e675c.css"
+  },
+  {
+    "revision": "078e1a35e536de8d7c3f",
+    "url": "/game-utilities-staging/css/chunk-575e8b02.be1a573c.css"
+  },
+  {
+    "revision": "e8cd5a9f6df0f94eeef4",
+    "url": "/game-utilities-staging/css/chunk-971b72ec.74196ff1.css"
+  },
+  {
+    "revision": "e88ca1f1b357006c4102",
+    "url": "/game-utilities-staging/css/chunk-e0eaed66.8597104c.css"
+  },
+  {
+    "revision": "e6d6259cf118a253fd95",
+    "url": "/game-utilities-staging/css/chunk-vendors.33fbaed9.css"
+  },
+  {
+    "revision": "2e9c71c7514c4036f0122b1bdc202c17",
+    "url": "/game-utilities-staging/img/en_flag.svg"
+  },
+  {
+    "revision": "c7f8bb0d07a3e1a92d9374be4bab1192",
+    "url": "/game-utilities-staging/img/si_flag.svg"
+  },
+  {
+    "revision": "373c3c397fdace8ce297bef3492cb905",
+    "url": "/game-utilities-staging/img/unique/unique_rules_1.svg"
+  },
+  {
+    "revision": "4d83d0daecad353ce69a81b47b47268b",
+    "url": "/game-utilities-staging/img/unique/unique_rules_2.svg"
+  },
+  {
+    "revision": "0c1f1eca1f68a7a9ec252e35d19b7aa0",
+    "url": "/game-utilities-staging/img/unique/unique_rules_3.svg"
+  },
+  {
+    "revision": "c8ad859373dd5ee9003f373f6ad4b9ef",
+    "url": "/game-utilities-staging/img/unique/unique_rules_4.svg"
+  },
+  {
+    "revision": "2463adeeffd22d9ae6aadc48c90592b1",
+    "url": "/game-utilities-staging/img/unique/unique_rules_5.svg"
+  },
+  {
+    "revision": "40b750f06c2b99fc7d01fa6e58f26aba",
+    "url": "/game-utilities-staging/index.html"
+  },
+  {
+    "revision": "9c3afc0959f643623293",
+    "url": "/game-utilities-staging/js/app.503b0461.js"
+  },
+  {
+    "revision": "671a7de38eea3dfaf916",
+    "url": "/game-utilities-staging/js/chunk-37587e1a.0d4fa875.js"
+  },
+  {
+    "revision": "078e1a35e536de8d7c3f",
+    "url": "/game-utilities-staging/js/chunk-575e8b02.3d516485.js"
+  },
+  {
+    "revision": "e8cd5a9f6df0f94eeef4",
+    "url": "/game-utilities-staging/js/chunk-971b72ec.0aa33208.js"
+  },
+  {
+    "revision": "e88ca1f1b357006c4102",
+    "url": "/game-utilities-staging/js/chunk-e0eaed66.e9b05013.js"
+  },
+  {
+    "revision": "e6d6259cf118a253fd95",
+    "url": "/game-utilities-staging/js/chunk-vendors.dc86e992.js"
+  },
+  {
+    "revision": "e7ec6f93c257388c53ee6cbdf5b07bfd",
+    "url": "/game-utilities-staging/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/game-utilities-staging/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-utilities-staging/precache-manifest.b0ed18336c0b19dd4fd2714d6d4a388c.js"
+  "/game-utilities-staging/precache-manifest.fc1281cad24ff11a501975c45399a33d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "game-utilities"});
